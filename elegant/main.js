4a7b2d007cf973e3eb2373b8126a34ce1a69bfed
Dingran Yuan <u7024641@anu.edu.au>
2020-07-04 16:36:39 +1000

    update link

---
+++ elegant/main.js
@@ -0,0 +1,26 @@
+alert('hi');
+console.log('This is an js log');
+//var var is globally scoped
+//let
+//const let and const have a block scope (you have to initialise the const)
+
+
+//let can let you reassign but for const you can't reassign
+let age = 30;
+console.log(age);
+
+
+//data type
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null; //is a variable but has nothing in it
+const y = undefined;
+let z;
+
+console.log(typeof x);
+
+//Concatentation
+console.log('My name is and I am');
+
