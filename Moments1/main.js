f382186597597bfc830715a988cdabba5072b587
Dingran Yuan <u7024641@anu.edu.au>
2020-07-07 01:58:20 +1000

    smaller size

--- Moments1/main.js
+++ Moments1/main.js
@@ -1,20 +1,11 @@
-window.onscroll = function() {
-    myFunction();
-}
 
 //Get the navbars
-navbar = document.getElementById("index-navbar")
+var o = document.getElementById("index-navbar");
 
 //get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var h = o.offsetHeight;
 
-//add the sticky calss to teh nav bar when you reach its scroll posotion. Remocve "sticky" when you leave the scroll posotion
-function myFunction() {
-    if(window.pageYOffset >= 0){
-        navbar.classList.add("sticky");
-    }
-    else{
-        navbar.classList.remove("sticky");
-    }
-}
+document.getElementsByClassName("fill-blank").style.marginTop=`${h}px`
+
+
 
